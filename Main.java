8a186b298cde7f36155d8cad686099aab9080b27
Jason Sundra <jasonsundra@gmail.com>
2025-11-21 11:42:50 +0800

    practice commit again

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
 public class Main{
     	int x = 5;
+        public static void main(String[] args){
+            Main myObj = new Main();
+            myObj.x = 30;
+            System.out.println(myObj.x);
+
+        }
 }
