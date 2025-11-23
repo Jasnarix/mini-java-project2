b21485c24cd556b7c8d031d16f765d0e1749623e
Jason Sundra <jasonsundra@gmail.com>
2025-11-23 09:55:14 +0800

    access modifiers

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
 public class Main{
-    int modelYear;
-    String modelName;
-
-    public Main(String modelName){
-        this(2020,modelName);
-    }
-    
-    public Main(int modelYear, String modelName){
-        this.modelYear = modelYear;
-        this.modelName = modelName;
-    }
-
-    public void printInfo() {
-        System.out.println(modelYear + " " + modelName);
-    }
-
     public static void main(String[] args){
-        Main car1 = new Main("Corvette");
-        Main car2 = new Main(1969,"Mazda");
-        car1.printInfo();
-        car2.printInfo();
+        Person p = new Person();
+        System.out.println(p.name);
+        System.out.println(p.age);
     }
 }
 
+class Person {
+    String name = "John Doe";
+    private int age = 32;
+}
