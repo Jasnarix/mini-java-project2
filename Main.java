febc0ff5b9aa2e5704ae86083c6c36b6020d3920
Jason Sundra <jasonsundra@gmail.com>
2025-11-23 10:26:00 +0800

    packages

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
+
+
 public class Main{
     public static void main(String[] args){
-        Person myObj = new Person();
-        myObj.setName("John");
-        System.out.println(myObj.getName());
+        Scanner myObj = new Scanner(System.in);
+
+
+        System.out.println("Enter username: ");
+        String userName = myObj.nextLine();
+        
+        System.out.println("Your username is: " + userName);
     }
 }
